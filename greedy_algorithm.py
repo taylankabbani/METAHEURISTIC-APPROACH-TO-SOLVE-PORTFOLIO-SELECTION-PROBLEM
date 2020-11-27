a68fe24958aa18c723bd57652412b7f9290d1931
taylankabbani <taylankabbani96@gmail.com>
2020-11-27 21:43:08 +0300

    Update 1.1

--- greedy_algorithm.py
+++ greedy_algorithm.py
@@ -1,5 +1,65 @@
-# Three stocks available, IBM, WMT and SEHI
-avg_return = [0.026, 0.008, 0.074]
-co_variance = {"IBM": [0.017087987, 0.003298885,0.001224849], "WMT":[0.003298885,0.005900944,0.004488271],
-               "SEHI":[0.001224849, 0.004488271, 0.063000818]}
-print(co_variance)
+import pandas as pd
+import numpy as np
+from itertools import combinations
+from toolz import valmap
+
+
+
+#Toy Example: Three stocks are available to buy IBM (IBM), Walmart (WMT), and Southern Electric (SEHI).
+monthly_price = pd.read_csv("Toy_dataset.txt",sep = " ",header=None, names = ["IBM","WMT","SEHI"], index_col=0)
+
+# Monthly return for each stock 
+monthly_return = monthly_price.rolling(2).apply(lambda x: (x[1]-x[0])/x[0]).dropna()
+
+# Average return (Expected return)
+avg_return = list(monthly_return.mean())
+
+#Covariance between each pair of stocks
+co_variance = pd.DataFrame.cov(monthly_return)
+
+# Risk aversion of the investor is set to 1, so the objective function will resemble the unconstrained one
+risk_lambda = 1
+
+# The total money to be invested in $ (Capital)
+capital = 1000
+
+# StDev of return for rach stock 
+stdev = list(monthly_return.std())
+
+# Sharpe_ration for each stock in an descending
+Sharpe_ratio = [[stock, Return/Std] for Return, Std, stock in zip(avg_return, stdev, monthly_price.columns)]
+Sharpe_ratio = sorted(Sharpe_ratio, key = lambda x: x[1], reverse=True)
+
+
+# Greedy function:
+def greedy(Sharpe_ratio, k, epsilon, delta, co_variance):
+    Best_solution = {}
+    V = 0
+    for i in range(10000):
+        # current solution
+        S = {}
+        # Random weights 
+        for stock in range(k):
+            S[Sharpe_ratio[stock][0]] = np.random.uniform(epsilon, delta)
+        # Normalizing to sum up to 1
+        S = valmap(lambda x: x/sum(S.values()), S)
+
+        # All possible combinations of stock pairs in the current solution
+        pair_comb = list(combinations(S.keys(), 2))
+
+        # Objective function value for the current solution
+        f = 0
+        for comb in pair_comb:
+            f += (float(co_variance[[comb[0]]].loc[comb[1]]) * (S[comb[0]]) * (S[comb[1]]))
+        
+        # update best solution
+        if f > V :
+            V = f
+            Best_solution = S
+
+    return Best_solution
+                    
+
+
+test = greedy(Sharpe_ratio, 3, 0.1, 1, co_variance)
+print(test)
